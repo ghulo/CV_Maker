4e5c71545315c45d7994d7e535e76cceb3664fcd
ghulo <labidaqaj@gmail.com>
2025-06-18 13:12:48 +0200

    Apply UI/UX fixes: Remove unused CSS, fix confirmation modal, adjust header styles.

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ const host = 'localhost';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/js/app.js'],
             refresh: true,
         }),
         vue(),
